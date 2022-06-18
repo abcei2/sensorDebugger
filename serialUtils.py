288600e84277fbbd65a4e6c62a09b82fc8ab5a4d
Jose B <jose.zdy@gmail.com>
2022-06-18 11:59:24 -0500

    live changes, format

--- serialUtils.py
+++ serialUtils.py
@@ -1,36 +1,45 @@
-
-import serial
-from serial.serialutil import SerialException
-import time
 import json
-from json.decoder import JSONDecodeError
+import serial
 import serial.tools.list_ports
+import time
 
+from json.decoder import JSONDecodeError
 from saveData import addData
+from serial.serialutil import SerialException
 
 sensorSerials = []
-AVAILABLE_DEVICES = ["/dev/ttyUSB0","/dev/ttyUSB1","/dev/ttyUSB2","COM3","COM4","COM5","COM6"]
+AVAILABLE_DEVICES = [
+    "/dev/ttyUSB0",
+    "/dev/ttyUSB1",
+    "/dev/ttyUSB2",
+    "COM3",
+    "COM4",
+    "COM5",
+    "COM6",
+]
 
 DEVICES = [
     {
-        "PORT":"/dev/ttyUSB0",
-        "BAUDRATE":57600,        
+        "PORT": "/dev/ttyUSB0",
+        "BAUDRATE": 9600,
     },
-    
-    {
-        "PORT":"/dev/ttyUSB1",
-        "BAUDRATE":9600,        
-    }
+    # {
+    #     "PORT":"/dev/ttyUSB1",
+    #     "BAUDRATE":9600,
+    # }
 ]
 
+
 def autoAddSensorSerials():
-    for p in list(serial.tools.list_ports.comports(include_links=True)):   
+    for p in list(serial.tools.list_ports.comports(include_links=True)):
         if p.device in AVAILABLE_DEVICES:
-            addSensorSerial(p.device)     
+            addSensorSerial(p.device)
+
 
 def manualAddSensorSerials():
-    for p in DEVICES:   
-        addSensorSerial(p["PORT"],p["BAUDRATE"])    
+    for p in DEVICES:
+        addSensorSerial(p["PORT"], p["BAUDRATE"])
+
 
 def sensorSerialExists(port):
     for sensorSerial in sensorSerials:
@@ -38,60 +47,65 @@ def sensorSerialExists(port):
             return True, sensorSerial
     return False, None
 
+
 def addSensorSerial(port, baudrate=9600):
     exists, sensorSerial = sensorSerialExists(port)
     created = False
-    if not exists:    
+    if not exists:
         try:
-            sensorSerial = serial.Serial(port=port,baudrate=baudrate, timeout=.1)  
+            sensorSerial = serial.Serial(port=port, baudrate=baudrate, timeout=0.1)
 
-            sensorSerial.reset_input_buffer()      
+            sensorSerial.reset_input_buffer()
             sensorSerials.append(sensorSerial)
             created = True
-            print("[INFO] Connected to port: ",port," baudrate: ",sensorSerial.baudrate)
+            print(
+                "[INFO] Connected to port: ", port, " baudrate: ", sensorSerial.baudrate
+            )
         except SerialException:
-            print("[ERROR] Can't connect to port: ",port)
-    
+            print("[ERROR] Can't connect to port: ", port)
+
     return created, exists, sensorSerial
+
 
 def sendCommand(command):
     exists, sensorSerial = sensorSerialExists(command["PORT"])
     if exists:
         try:
-            sensorSerial.write(bytes(json.dumps(command), 'utf-8'))               
-            print("[INFO] Command send")          
+            sensorSerial.write(bytes(json.dumps(command), "utf-8"))
+            print("[INFO] Command send")
         except SerialException:
             sensorSerial.close()
-            sensorSerials.remove(sensorSerial)         
+            sensorSerials.remove(sensorSerial)
     else:
         print("[ERROR] Port doen't exists")
 
+
 # read serial of all devices each 0.1 secs
-def serialReadLoop():    
+def serialReadLoop():
     while True:
-        manualAddSensorSerials()
-        for sensorSerial in sensorSerials:   
+        autoAddSensorSerials()
+        for sensorSerial in sensorSerials:
             data = ""
-            while sensorSerial.in_waiting>0:   
-                if sensorSerial.in_waiting>4000:
-                    sensorSerial.reset_input_buffer()    
+            while sensorSerial.in_waiting > 0:
+                if sensorSerial.in_waiting > 400:
+                    sensorSerial.reset_input_buffer()
                     continue
-                try:  
+                try:
                     print(f"[INFO BYTES] ", sensorSerial.in_waiting)
-                    data = sensorSerial.readline()       
-                    data  = json.loads(data)
+                    data = sensorSerial.readline()
+                    data = json.loads(data)
                     print(f"[SERIAL {sensorSerial.port}] ", data)
-                    if 'WHOAMI' in data.keys() and 'TASK' in data.keys():
+                    if "WHOAMI" in data.keys() and "TASK" in data.keys():
                         addData(data)
                     print("-----------------------")
                 except JSONDecodeError:
-                    print(f"[ERROR {sensorSerial.port}] Formato de json erroneo.",data)
+                    print(f"[ERROR {sensorSerial.port}] Formato de json erroneo.", data)
                     print("-----------------------")
-                    pass                
+                    pass
                 except SerialException:
                     print(f"[ERROR {sensorSerial.port}] Puerto desconectado.")
                     print("-----------------------")
                     sensorSerial.close()
                     sensorSerials.remove(sensorSerial)
-                
-        time.sleep(0.1)
+
+        time.sleep(0.01)
